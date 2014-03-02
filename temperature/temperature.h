d7f6057c0b24efc7f6394e94afa3d832505da44b
Ed Kademan <ed@kademan.org>
2014-03-02 15:49:16 -0500

    Revert "modify header for dst changes"
    This reverts commit 77ab1a3d1b07bcffd2ccfacb1f99768f1a175bac.

--- temperature/temperature.h
+++ temperature/temperature.h
@@ -1,8 +1,5 @@
 #define MAXBUFFER 50
 
-enum sunday
-  {firstSunday, secondSunday, thirdSunday, fourthSunday, lastSunday}
-
 typedef struct {
   int n;			/* number of characters in buffer */
   char buf[MAXBUFFER];
@@ -13,8 +10,3 @@ typedef struct {
   char description[MAXBUFFER];
   DallasTemperature* ds;
 } therm;
-
-typedef struct {
-  int mon;
-  sunday sun;
-} dstdef;
